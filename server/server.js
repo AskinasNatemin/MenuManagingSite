cb2245f1620cef4ecc10e9d3749569fcb0a62fd6
AskinasNatemin <askinas2002@gmail.com>
2025-03-22 08:09:44 +0530

    change in cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 const corsOption={
-  origin:'https://menu-managing-site.vercel.app',
+  origin:process.env.APPLICATION_URL,
   method:"GET,HEAD,PUT,PATCH,DELETE,POST"
 }
 app.use(cors(corsOption));
